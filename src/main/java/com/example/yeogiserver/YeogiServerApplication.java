df0d70492d4a1e84eaa2ed0d148a1adab2d8359b
DK <laancer4@gmail.com>
2024-05-31 20:28:33 +0900

    init

---
+++ src/main/java/com/example/yeogiserver/YeogiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.yeogiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeogiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeogiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yeogiserver/YeogiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yeogiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeogiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
