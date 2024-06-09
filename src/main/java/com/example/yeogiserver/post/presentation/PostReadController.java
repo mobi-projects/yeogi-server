6fc0805d02cfe07ce205ace1f0e4e99853a1a03b
DK <laancer4@gmail.com>
2024-06-09 19:01:24 +0900

    fix : 누락된 uri 추가

--- src/main/java/com/example/yeogiserver/post/presentation/PostReadController.java
+++ src/main/java/com/example/yeogiserver/post/presentation/PostReadController.java
@@ -3,6 +3,8 @@ package com.example.yeogiserver.post.presentation;
 import com.example.yeogiserver.post.application.PostReadService;
 import com.example.yeogiserver.post.application.dto.PostResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +13,8 @@ public class PostReadController {
 
     private final PostReadService postReadService;
 
-    public PostResponseDto getPostById(Long postId) {
+    @GetMapping("/posts/{postId}")
+    public PostResponseDto getPostById(@PathVariable Long postId) {
         return postReadService.getPostById(postId);
     }
 }
