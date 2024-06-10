66ee3a4639a566a0d6469c790abd8fcdc98d9891
Kwonnamhyung <87262714+Kwonnamhyung@users.noreply.github.com>
2024-06-10 23:37:58 +0900

    Yg 50 feat oath 2 (#3)
    git commit -m "YG-50 #beclose #comment OAuth2 인증"

---
+++ src/main/java/com/example/yeogiserver/security/domain/OAuth2UserInfo.java
@@ -0,0 +1,70 @@
+package com.example.yeogiserver.security.domain;
+
+import com.example.yeogiserver.common.exception.CustomException;
+import com.example.yeogiserver.common.exception.ErrorCode;
+import com.example.yeogiserver.member.domain.Gender;
+import com.example.yeogiserver.member.domain.Member;
+import com.example.yeogiserver.member.domain.Role;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Builder
+@Getter
+public class OAuth2UserInfo {
+
+    private String name;
+    private String email;
+    private Gender gender;
+    private String ageRange;
+    private String profile;
+
+    public static OAuth2UserInfo of(String registrationId , Map<String, Object> attributes) {
+         switch (registrationId) {
+            case "google" :
+                return ofGoogle(attributes);
+             case "naver" :
+                 return ofNaver(attributes);
+             case "kakao" :
+                 return ofKakao(attributes);
+            default :
+                throw new CustomException(ErrorCode.ILLEGAL_REGISTRATION_ID);
+        }
+    }
+
+    private static OAuth2UserInfo ofGoogle(Map<String, Object> attributes) {
+        return OAuth2UserInfo.builder()
+                .name(String.valueOf(attributes.get("name")))
+                .email(String.valueOf(attributes.get("email")))
+                .profile(String.valueOf(attributes.get("picture")))
+                .build();
+    }
+
+    private static OAuth2UserInfo ofNaver(Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuth2UserInfo.builder()
+                .name(String.valueOf(response.get("nickname")))
+                .email(String.valueOf(response.get("email")))
+                .profile(String.valueOf(response.get("profile_image")))
+                .gender(String.valueOf(response.get("gender")).equals("M") ? Gender.M : Gender.F)
+                .ageRange(String.valueOf(response.get("age")))
+                .build();
+    }
+
+    private static OAuth2UserInfo ofKakao(Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        return OAuth2UserInfo.builder()
+                .name(String.valueOf(kakaoProfile.get("nickname")))
+                .email(String.valueOf(kakaoAccount.get("email")))
+                .profile(String.valueOf(kakaoProfile.get("profile_image_url")))
+                .build();
+    }
+
+    public Member toEntity() {
+        return new Member(null , email , null , name , ageRange , profile , gender , Role.USER);
+    }
+
+}
---
+++ src/test/java/com/example/yeogiserver/common/JasyptConfigTest.java
@@ -0,0 +1,27 @@
+package com.example.yeogiserver.common;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JasyptConfigTest {
+
+    @DisplayName("jasypt 암호화")
+    @Test
+    void jasyptEncryptor() {
+
+        String key = "";
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setPoolSize(8);   // 코어 수
+        encryptor.setPassword(key);
+        encryptor.setAlgorithm("PBEWithMD5AndTripleDES");
+
+        String str = "";
+        String encrypt = encryptor.encrypt(str);
+        String decrypt = encryptor.decrypt(encrypt);
+
+        System.out.println("encrypt = " + encrypt);
+        System.out.println("decrypt = " + decrypt);
+    }
+}
