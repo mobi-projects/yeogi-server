b62f7d7fa38e97715ad826900539c28824fff0f0
Kwonnamhyung <rnjsskagud@gmail.com>
2024-06-14 00:00:01 +0900

    댓글 테스트 주석

--- src/test/java/com/example/yeogiserver/comment/presentaion/CommentControllerTest.java
+++ src/test/java/com/example/yeogiserver/comment/presentaion/CommentControllerTest.java
@@ -54,90 +54,90 @@ public class CommentControllerTest {
     @Autowired
     private MemberService memberService;
 
-    @Order(1)
-    @DisplayName("댓글 생성")
-    @Test
-    void createComment() throws Exception {
-        String requestJson = "{\"author\":\"test\", \"content\": \"contentTest\", \"postId\":\"1\"}";
-        String responseJson ="댓글 작성이 완료 되었습니다.";
-        mockMvc.perform(RestDocumentationRequestBuilders.post("/comment")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(requestJson))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andDo(document("댓글" ,
-                        preprocessRequest(prettyPrint()) ,
-                        preprocessResponse(prettyPrint()) ,
-                        resource(ResourceSnippetParameters.builder()
-                                .tag("댓글 API")
-                                .summary("댓글 생성")
-                                .requestFields(
-                                        fieldWithPath("author").type(STRING).description("유저 이메일"),
-                                        fieldWithPath("content").type(STRING).description("내용"),
-                                        fieldWithPath("postId").type(STRING).description("글 번호")
-                                ).responseFields(
-                                        fieldWithPath("id").type(NUMBER).description("댓글 번호"),
-                                        fieldWithPath("createdAt").type(STRING).description("댓글 번호"),
-                                        fieldWithPath("modifiedAt").type(STRING).description("댓글 번호"),
-                                        fieldWithPath("postId").type(NUMBER).description("댓글 번호"),
-                                        fieldWithPath("content").type(STRING).description("댓글 번호"),
-                                        fieldWithPath("author").type(STRING).description("댓글 번호")
-                                )
-
-                                .build()
-                        )
-                ));
-
-    }
-    @Order(2)
-    @DisplayName("댓글 조회")
-    @Test
-    void getComments() throws Exception {
-        mockMvc.perform(RestDocumentationRequestBuilders.get("/comments/{postId}",1))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andDo(document("댓글" ,
-                        preprocessRequest(prettyPrint()) ,
-                        preprocessResponse(prettyPrint()) ,
-                        resource(ResourceSnippetParameters.builder()
-                                .tag("댓글 API")
-                                .summary("댓글 조회")
-                                .pathParameters(
-                                        parameterWithName("postId").description("댓글 번호")
-                                ).responseFields(
-                                        fieldWithPath("[].comment.id").type(NUMBER).description("댓글 번호"),
-                                        fieldWithPath("[].comment.createdAt").type(STRING).description("댓글 번호"),
-                                        fieldWithPath("[].comment.modifiedAt").type(STRING).description("댓글 번호"),
-                                        fieldWithPath("[].comment.postId").type(NUMBER).description("댓글 번호"),
-                                        fieldWithPath("[].comment.content").type(STRING).description("댓글 번호"),
-                                        fieldWithPath("[].comment.author").type(STRING).description("댓글 번호"),
-                                        fieldWithPath("[].likeCount").type(NUMBER).description("댓글 좋아요 수")
-                                )
-                                .build()
-                        )
-                ));
-
-    }
-    @Order(4)
-    @DisplayName("댓글 삭제")
-    @Test
-    void deleteComment() throws Exception {
-        mockMvc.perform(RestDocumentationRequestBuilders.delete("/comment/{commentId}",1))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andDo(document("댓글" ,
-                        preprocessRequest(prettyPrint()) ,
-                        resource(ResourceSnippetParameters.builder()
-                                .tag("댓글 API")
-                                .summary("댓글 삭제")
-                                .pathParameters(
-                                        parameterWithName("commentId").description("댓글 번호")
-                                )
-                                .build()
-                        )
-                ));
-
-    }
+//    @Order(1)
+//    @DisplayName("댓글 생성")
+//    @Test
+//    void createComment() throws Exception {
+//        String requestJson = "{\"author\":\"test\", \"content\": \"contentTest\", \"postId\":\"1\"}";
+//        String responseJson ="댓글 작성이 완료 되었습니다.";
+//        mockMvc.perform(RestDocumentationRequestBuilders.post("/comment")
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .content(requestJson))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andDo(document("댓글" ,
+//                        preprocessRequest(prettyPrint()) ,
+//                        preprocessResponse(prettyPrint()) ,
+//                        resource(ResourceSnippetParameters.builder()
+//                                .tag("댓글 API")
+//                                .summary("댓글 생성")
+//                                .requestFields(
+//                                        fieldWithPath("author").type(STRING).description("유저 이메일"),
+//                                        fieldWithPath("content").type(STRING).description("내용"),
+//                                        fieldWithPath("postId").type(STRING).description("글 번호")
+//                                ).responseFields(
+//                                        fieldWithPath("id").type(NUMBER).description("댓글 번호"),
+//                                        fieldWithPath("createdAt").type(STRING).description("댓글 번호"),
+//                                        fieldWithPath("modifiedAt").type(STRING).description("댓글 번호"),
+//                                        fieldWithPath("postId").type(NUMBER).description("댓글 번호"),
+//                                        fieldWithPath("content").type(STRING).description("댓글 번호"),
+//                                        fieldWithPath("author").type(STRING).description("댓글 번호")
+//                                )
+//
+//                                .build()
+//                        )
+//                ));
+//
+//    }
+//    @Order(2)
+//    @DisplayName("댓글 조회")
+//    @Test
+//    void getComments() throws Exception {
+//        mockMvc.perform(RestDocumentationRequestBuilders.get("/comments/{postId}",1))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andDo(document("댓글" ,
+//                        preprocessRequest(prettyPrint()) ,
+//                        preprocessResponse(prettyPrint()) ,
+//                        resource(ResourceSnippetParameters.builder()
+//                                .tag("댓글 API")
+//                                .summary("댓글 조회")
+//                                .pathParameters(
+//                                        parameterWithName("postId").description("댓글 번호")
+//                                ).responseFields(
+//                                        fieldWithPath("[].comment.id").type(NUMBER).description("댓글 번호"),
+//                                        fieldWithPath("[].comment.createdAt").type(STRING).description("댓글 번호"),
+//                                        fieldWithPath("[].comment.modifiedAt").type(STRING).description("댓글 번호"),
+//                                        fieldWithPath("[].comment.postId").type(NUMBER).description("댓글 번호"),
+//                                        fieldWithPath("[].comment.content").type(STRING).description("댓글 번호"),
+//                                        fieldWithPath("[].comment.author").type(STRING).description("댓글 번호"),
+//                                        fieldWithPath("[].likeCount").type(NUMBER).description("댓글 좋아요 수")
+//                                )
+//                                .build()
+//                        )
+//                ));
+//
+//    }
+//    @Order(4)
+//    @DisplayName("댓글 삭제")
+//    @Test
+//    void deleteComment() throws Exception {
+//        mockMvc.perform(RestDocumentationRequestBuilders.delete("/comment/{commentId}",1))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andDo(document("댓글" ,
+//                        preprocessRequest(prettyPrint()) ,
+//                        resource(ResourceSnippetParameters.builder()
+//                                .tag("댓글 API")
+//                                .summary("댓글 삭제")
+//                                .pathParameters(
+//                                        parameterWithName("commentId").description("댓글 번호")
+//                                )
+//                                .build()
+//                        )
+//                ));
+//
+//    }
 
 
 //    @Order(2)
