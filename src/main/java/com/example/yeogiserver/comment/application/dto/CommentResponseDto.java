0230d9518bb3b9ddd421be10597b63d1e37e8dec
choeyejun <inVeryiIrLFVhNy9U7ltXktfxS4IFstgY4avstXTUE8=>
2024-06-16 20:17:58 +0900

    fix : Comment CreateAt, ModifiyAt Addd

--- src/main/java/com/example/yeogiserver/comment/application/dto/CommentResponseDto.java
+++ src/main/java/com/example/yeogiserver/comment/application/dto/CommentResponseDto.java
@@ -2,13 +2,15 @@ package com.example.yeogiserver.comment.application.dto;
 
 import com.example.yeogiserver.comment.domain.Comment;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record CommentResponseDto(Long id, String content, String nickname, Long postId, Long likeCount) {
+public record CommentResponseDto(Long id, String content, String nickname, Long postId, LocalDateTime createdAt, LocalDateTime modifiedAt, Long likeCount) {
     public static List<CommentResponseDto> toEntityList(List<Comment> comments, Long likeCount) {
         return comments.stream()
-                .map(comment -> new CommentResponseDto(comment.getId(),comment.getContent(),comment.getMember().getNickname(),comment.getPost().getId(), likeCount))
+                .map(comment -> new CommentResponseDto(comment.getId(),comment.getContent()
+                        ,comment.getMember().getNickname(),comment.getPost().getId(),comment.getCreatedAt(),comment.getModifiedAt(), likeCount))
                 .collect(Collectors.toList());
     }
 
