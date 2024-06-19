ec2bbd7316415eb43cc1f2623eb2f0df45d97258
Kwonnamhyung <rnjsskagud@gmail.com>
2024-06-19 22:57:34 +0900

    exceptionAdvice 수정

--- src/main/java/com/example/yeogiserver/base/presentation/advice/GlobalControllerAdvice.java
+++ src/main/java/com/example/yeogiserver/base/presentation/advice/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.yeogiserver.base.presentation.advice;
 
+import com.example.yeogiserver.common.exception.CustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +11,13 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleException(RuntimeException e) {
+
+        if(e instanceof CustomException) {
+            return ResponseEntity
+                    .status(((CustomException) e).getErrorCode().getStatus())
+                    .body(new ErrorResponse(((CustomException) e).getErrorCode().getMessage() , HttpStatus.valueOf(((CustomException) e).getErrorCode().getStatus())));
+        }
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
