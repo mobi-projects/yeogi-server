99ea90f7b96167567faecb18d194a5da1e3e45f7
DK <laancer4@gmail.com>
2024-07-04 23:25:55 +0900

    feat : 토큰 만료 시간, 필터 타는 매서드 예외 설정

--- src/main/java/com/example/yeogiserver/post/presentation/PostReadController.java
+++ src/main/java/com/example/yeogiserver/post/presentation/PostReadController.java
@@ -6,7 +6,6 @@ import com.example.yeogiserver.post.application.dto.response.PostResponseDto;
 import com.example.yeogiserver.post.domain.Theme;
 import com.example.yeogiserver.post.presentation.search_condition.PostSearchType;
 import com.example.yeogiserver.post.presentation.search_condition.PostSortCondition;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@SecurityRequirement(name = "Bearer Authentication")
 @RequiredArgsConstructor
 public class PostReadController {
 
