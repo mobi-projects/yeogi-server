6a33347a84b66f0ebc0d4343d21567c5eb177637
DK <laancer4@gmail.com>
2024-07-07 12:03:25 +0900

    test : 테스트 의존성(S3) 추가 및 assertion 정리

--- src/test/java/com/example/yeogiserver/post/repository/DefaultPostReadRepositoryTest.java
+++ src/test/java/com/example/yeogiserver/post/repository/DefaultPostReadRepositoryTest.java
@@ -7,19 +7,18 @@ import com.example.yeogiserver.member.repository.MemberJpaRepository;
 import com.example.yeogiserver.post.domain.Post;
 import com.example.yeogiserver.post.domain.PostTheme;
 import com.example.yeogiserver.post.domain.Theme;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
@@ -58,9 +57,13 @@ class DefaultPostReadRepositoryTest {
 
         Optional<Post> expected = jpaPostRepository.findFirstByPostThemeListThemeOrderByViewCountDescCreatedAtDesc(Theme.ACTIVITY);
 
-        assertThat(post.getViewCount()).isEqualTo(2);
-        assertThat(expected.isPresent()).isTrue();
-        assertThat(expected.get().getId()).isEqualTo(post.getId());
+        SoftAssertions.assertSoftly(
+                softly -> {
+                    softly.assertThat(post.getViewCount()).isEqualTo(2);
+                    softly.assertThat(expected.isPresent()).isTrue();
+                    softly.assertThat(expected.get().getId()).isEqualTo(post.getId());
+                }
+        );
     }
 
     @Test
@@ -79,9 +82,13 @@ class DefaultPostReadRepositoryTest {
         Optional<Post> expected = jpaPostRepository.findFirstByPostThemeListThemeOrderByViewCountDescCreatedAtDesc(Theme.ACTIVITY);
 
 
-        assertThat(expected.isPresent()).isTrue();
-        assertThat(post2.getCreatedAt().isAfter(post.getCreatedAt())).isTrue();
-        assertThat(expected.get().getId()).isEqualTo(post2.getId());
+        SoftAssertions.assertSoftly(
+                softly -> {
+                    softly.assertThat(expected.isPresent()).isTrue();
+                    softly.assertThat(post2.getCreatedAt().isAfter(post.getCreatedAt())).isTrue();
+                    softly.assertThat(expected.get().getId()).isEqualTo(post2.getId());
+                }
+        );
     }
 
     @Test
@@ -128,52 +135,61 @@ class DefaultPostReadRepositoryTest {
             }
         }
 
-        assertThat(resultSet).hasSize(3);
-        assertThat(resultSet).contains(post, post2, post3);
+        SoftAssertions.assertSoftly(
+                softly -> {
+                    softly.assertThat(resultSet).hasSize(3);
+                    softly.assertThat(resultSet).contains(post, post2, post3);
+                }
+        );
+
     }
 
-//    @Test
-//    @DisplayName("만약 다른 테마에서 이미 발견한 포스트라면, 응답 포스트 리스트에 추가되지 않는다.")
-//    void shouldReturnDummyIfPostNotExists() {
-//        Post post = PostFixture.createDefaultFixtureOf(testMemberFixture);
-//        Post post2 = PostFixture.createDefaultFixtureOf(testMemberFixture);
-//
-//        List<Theme> themeList = List.of(Theme.ACTIVITY, Theme.EATING, Theme.PACKAGE);
-//        List<PostTheme> postThemes = themeList.stream().map(PostTheme::new).toList();
-//        List<PostTheme> postThemes2 = themeList.stream().map(PostTheme::new).toList();
-//
-//        post.replaceThemeList(postThemes);
-//        PostTheme postTheme = postThemes2.getFirst();
-//        post2.replaceThemeList(List.of(postTheme));
-//
-//        jpaPostRepository.save(post);
-//        jpaPostRepository.save(post2);
-//
-//        List<Post> resultSet = new ArrayList<>();
-//        Post dummy = new Post();
-//
-//        for (Theme theme : themeList) {
-//            List<Post> list = jpaPostRepository.findAllByPostThemeListThemeOrderByViewCountDescCreatedAtDesc(PageRequest.of(0, 10), theme).stream().toList();
-//            if (list.isEmpty()){
-//                resultSet.add(dummy); // 더미 포스트 추가
-//                continue;
-//            }
-//
-//            boolean found = false;
-//            for (Post each : list) {
-//                if (!resultSet.contains(each)) {
-//                    resultSet.add(each);
-//                    found = true;
-//                    break;
-//                }
-//            }
-//
-//            if (!found) {
-//                resultSet.add(dummy); // 더미 포스트 추가
-//            }
-//        }
-//
-//        assertThat(resultSet).hasSize(3);
-//        assertThat(resultSet).contains(post, post2, dummy);
-//    }
+    @Test
+    @DisplayName("테마에 해당하는 포스트가 없다면 더미를 리턴한다.")
+    void shouldReturnDummyIfPostNotExists() {
+        Post post = PostFixture.createDefaultFixtureOf(testMemberFixture);
+        Post post2 = PostFixture.createDefaultFixtureOf(testMemberFixture);
+
+        List<Theme> themeList = List.of(Theme.ACTIVITY, Theme.EATING, Theme.PACKAGE);
+        List<PostTheme> postThemes = themeList.stream().map(PostTheme::new).toList();
+        List<PostTheme> postThemes2 = themeList.stream().map(PostTheme::new).toList();
+
+        post.replaceThemeList(postThemes);
+        PostTheme postTheme = postThemes2.getFirst();
+        post2.replaceThemeList(List.of(postTheme));
+
+        jpaPostRepository.save(post);
+        jpaPostRepository.save(post2);
+
+        List<Post> resultSet = new ArrayList<>();
+        Post dummy = new Post();
+
+        for (Theme theme : themeList) {
+            List<Post> list = jpaPostRepository.findAllByPostThemeListThemeOrderByViewCountDescCreatedAtDesc(PageRequest.of(0, 10), theme).stream().toList();
+            if (list.isEmpty()){
+                resultSet.add(dummy); // 더미 포스트 추가
+                continue;
+            }
+
+            boolean found = false;
+            for (Post each : list) {
+                if (!resultSet.contains(each)) {
+                    resultSet.add(each);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                resultSet.add(dummy); // 더미 포스트 추가
+            }
+        }
+
+        SoftAssertions.assertSoftly(
+                softly -> {
+                    softly.assertThat(resultSet).hasSize(3);
+                    softly.assertThat(resultSet).contains(post, post2, dummy);
+                }
+        );
+    }
 }
