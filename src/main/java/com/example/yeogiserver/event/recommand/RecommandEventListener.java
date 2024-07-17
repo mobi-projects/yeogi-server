733033491dbe45321d9fa35bcd854aa90be4133b
choeyejun <inVeryiIrLFVhNy9U7ltXktfxS4IFstgY4avstXTUE8=>
2024-07-17 16:47:26 +0900

    feat : 추천 글 조회 추가

--- src/main/java/com/example/yeogiserver/event/recommand/RecommandEventListener.java
+++ src/main/java/com/example/yeogiserver/event/recommand/RecommandEventListener.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -19,7 +20,7 @@ public class RecommandEventListener {
     private final TransactionTemplate transactionTemplate;
 
     @EventListener
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @Async
     public void handleRecommandEvent(RecommandEvent recommandEvent) {
         recommandService.saveRecommand(recommandEvent);
