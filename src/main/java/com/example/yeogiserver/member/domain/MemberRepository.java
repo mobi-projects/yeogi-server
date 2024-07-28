722fc3e0ab214b42633fb8098cf2bf59ca1774b0
choeyejun <inVeryiIrLFVhNy9U7ltXktfxS4IFstgY4avstXTUE8=>
2024-07-28 10:30:40 +0900

    refactor : Service에서 구현체가 아닌 인터페이스를 주입
    1. 인터페이스를 사용하여 리포지토리를 주입하는 방식을 채택하여 개발을 진행하였는데, 인터페이스가 아닌 구현체를 직접 주입하게 되면 해당 장점을 잃어버림
    2. DIP 원칙 위반

--- src/main/java/com/example/yeogiserver/member/domain/MemberRepository.java
+++ src/main/java/com/example/yeogiserver/member/domain/MemberRepository.java
@@ -2,10 +2,17 @@ package com.example.yeogiserver.member.domain;
 
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository {
 
     Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByEmail(String email);
+
+    void delete(String email);
 
     List<Member> findAllByIds(List<Long> memberIds);
 
