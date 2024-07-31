7a2d9d8cd241bec429972159b1298399318a778a
DK <laancer4@gmail.com>
2024-07-31 17:39:57 +0900

    fix : removed error log

--- src/main/java/com/example/yeogiserver/security/config/JwtTokenProvider.java
+++ src/main/java/com/example/yeogiserver/security/config/JwtTokenProvider.java
@@ -169,14 +169,12 @@ public class JwtTokenProvider {
 
     public Claims parseClaims(String token) {
         try {
-            Claims body = Jwts.parserBuilder()
+            return Jwts.parserBuilder()
                     .setSigningKey(key)
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
-            return body;
         } catch (Exception e){
-            log.error("invalid token : {}", token);
             throw new IllegalArgumentException("invalid token : " + token);
         }
     }
