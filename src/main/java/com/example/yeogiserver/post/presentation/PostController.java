8ebb8b376539b0af083dcae7c748263ec8e2120a
Kwonnamhyung <rnjsskagud@gmail.com>
2024-08-06 21:28:28 +0900

    logging 제거

--- src/main/java/com/example/yeogiserver/post/presentation/PostController.java
+++ src/main/java/com/example/yeogiserver/post/presentation/PostController.java
@@ -43,8 +43,6 @@ public class PostController {
                 .buildAndExpand(postId)
                 .toUri();
 
-        log.info("[CREATE POST] TIME = {} , ID = {} , USER = {}" , commonService.getTime() , postId , commonService.getName(userDetails));
-
         // 201 Created 응답과 Location 헤더를 반환
         return ResponseEntity.created(location).build();
     }
@@ -53,34 +51,29 @@ public class PostController {
     @Operation(description = "postId 에 해당하는 게시글의 조회수를 추가 한다.")
     public void addViewCount(@PathVariable Long postId , @AuthenticationPrincipal CustomUserDetails userDetails){
         postService.addViewCount(postId);
-        log.info("[ADD VIEW COUNT] TIME = {} , ID = {} , USER = {} " , commonService.getTime() , postId , commonService.getName(userDetails));
     }
 
     @PutMapping("/posts/{postId}")
     @Operation(description = "postId 에 해당하는 게시글을 수정한다.")
     public void updatePost(@PathVariable Long postId, @RequestBody PostUpdateRequest postRequestDto, @AuthenticationPrincipal CustomUserDetails userDetails) {
         postService.updatePost(postId, postRequestDto, userDetails.getId());
-        log.info("[UPDATE POST] TIME = {} , ID = {} , USER = {} " , commonService.getTime() , postId , commonService.getName(userDetails));
     }
 
     @DeleteMapping("/posts/{postId}")
     @Operation(description = "postId 에 해당하는 게시글을 삭제한다.")
     public void deletePost(@PathVariable Long postId , @AuthenticationPrincipal CustomUserDetails userDetails) {
         postService.delete(postId , userDetails.getId());
-        log.info("[DELETE POST] TIME = {} , ID = {} , USER = {} " , commonService.getTime() , postId , commonService.getName(userDetails));
     }
 
     @PostMapping("/posts/{postId}/likes")
     @Operation(description = "postId 에 해당하는 게시글의 좋아요를 생성한다.")
     public void likePost(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable Long postId) {
         postService.likePost(userDetails.getId(), postId);
-        log.info("[LIKE POST] TIME = {} , ID = {} , USER = {} " , commonService.getTime() , postId , commonService.getName(userDetails));
     }
 
     @DeleteMapping("/posts/{postId}/likes")
     @Operation(description = "postId 에 해당하는 게시글의 좋아요를 삭제한다.")
     public void unlikePost(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable Long postId) {
         postService.dislikePost(userDetails.getId(), postId);
-        log.info("[DISLIKE POST] TIME = {} , ID = {} , USER = {} " , commonService.getTime() , postId , commonService.getName(userDetails));
     }
 }
